826bb645129ada79ef72257efa2a59325b44cd5e
bigoo34 <95050996+bigoo34@users.noreply.github.com>
2022-01-27 19:25:04 +0200

    Update nodejs_docker.groovy

--- basics/dsl/nodejs_docker.groovy
+++ basics/dsl/nodejs_docker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/yanivomc/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/bigoo34/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@devophift.work')
         }
@@ -12,9 +12,9 @@ job('NodeJS Docker example') {
     
     steps {
         dockerBuildAndPublish {
-            repositoryName('yanivomc/amdocsapp')
+            repositoryName('bigoo/playground')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('yaniv-dockerhub')
+            registryCredentials('bigoo-dockerhub')
             buildContext('./basics/')
             forcePull(false)
             forceTag(false)
